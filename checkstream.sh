2972f9dbf56db572582132a7dbbdbb184865790e
chinatsun <lolexplode@gmail.com>
2014-02-20 10:52:05 +0100

    Update checkstream.sh

--- checkstream.sh
+++ checkstream.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # checkstream.sh
 # Usage: `./checkstream.sh chinatsun` will open http://www.twitch.tv/chinatsun's stream
+# Someone asked me if livestreamer could be used as a cronjob for archiving livestreams from twitch, but I feel like that might be a little clunky. I decided to write something simple that wouldn't spawn livestreamer until it really knows that it's necessary.
 
 if [ -z "$1" ]
 	then
@@ -8,13 +9,11 @@ if [ -z "$1" ]
 		exit 1
 fi
 if ( ps aux | grep -q "[le]ivestreamer .*$1" ) # tips fedora
-	then
 		echo "Already watching."
 		exit 1
 fi
 
-API=$(curl -is https://api.twitch.tv/kraken/streams/$1/) # Let's look at twitch.tv's API. Not yet sure if it's a good idea to have it stored in a var like that.
-RIGHTNOW=$(date -Iminutes | sed -e 's/+[0-9]*$//g' -e 's/[:]/-/g') # Used as a filenaming scheme for archiving streams if that's what you want.
+API=$(curl -is https://api.twitch.tv/kraken/streams/"$1"/) # Let's look at twitch.tv's API. Not yet sure if it's a good idea to have it stored in a var like that.
 
 if ( echo "$API" | grep -q "does not exist" )
 	then 
@@ -37,6 +36,11 @@ if ( echo "$API" | grep -q "\"stream\":null" )
 		exit 1
 fi
 
+# You can probably uncomment this if you want to run multiple instances at once, i.e. archiving multiple streams or whatever. I haven't tried myself, though.
 killall livestreamer 2>/dev/null;
-#echo "Dumping to $1-$RIGHTNOW" # Uncomment this and the stuff on the next line in order to use this as a stream archiving tool.
-livestreamer twitch.tv/$1 medium # -o $1-$RIGHTNOW # A good idea might be to specify the path to where you want the streams to be saved to. i.e. -o /home/Cockbot/streams/$1-$RIGHTNOW
+
+# Uncomment for archiving streams.
+#RIGHTNOW=$(date -Iminutes | sed -e 's/+[0-9]*$//g' -e 's/[:]/-/g'); echo "Dumping to $1-$RIGHTNOW"; livestreamer twitch.tv/"$1" best -o $1-$RIGHTNOW;
+
+# Also consider commenting this if you're going to use the line above.
+livestreamer twitch.tv/"$1" medium
