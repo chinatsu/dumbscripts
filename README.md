22f09b46e5d9b06171a6d949bda28ac720fa394e
chinatsu <lolexplode@gmail.com>
2015-03-15 19:34:08 +0100

    Add simple game search functionality.

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Scripts for various purposes.
 
 ##srl.py
 A simple script that looks up the http://speedrunslive.com API and outputs the top `n` streamers. `n` is determined by your tty's height.
+You can search for a game with the `--game` flag, there are a few aliases for some popular games. For example: `./srl.py --game sm64` will output Super Mario 64 streams.
+`./srl.py --game sm64` also equals `./srl.py --game 'super mario 64'`. You can view all the current aliases near the top of the script.
 
 ##checkstream.sh
 A script that checks the http://twitch.tv API if a specified stream is online. Once the API successfully goes through some checks (if the stream is already open, if it's offline/banned, etc.), it will spawn a livestreamer instance. I did it this way for the sake of a cronjob scenario. Since livestreamer has the option to output the stream into a file, one can easily use it for archiving streams for watching later.
--- srl.py
+++ srl.py
@@ -2,7 +2,11 @@
 import requests
 import subprocess
 import os
+import argparse
 from html import parser
+
+#TODO
+# Some browsing functionality
 
 # config
 livestreamerQuality = 'medium, source'
@@ -10,6 +14,32 @@ livestreamerQuality = 'medium, source'
 # not config
 tHeight = int((int(os.popen('stty size', 'r').read().split()[0]) - 4) / 2)
 h = parser.HTMLParser()
+gameAliases = {
+               'gta': 'grand theft auto',
+               'sm64': 'super mario 64',
+               'sms': 'super mario sunshine',
+               'alttp': 'a link to the past',
+               'oot': 'ocarina of time',
+               'tww': 'the wind waker',
+               'sm': 'super metroid',
+               'tes': 'elder scrolls',
+               'das': 'dark souls',
+               'dk': 'donkey kong',
+               'ff': 'final fantasy'
+               }
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--game', help='Displays streams with matching string in games', nargs='?', default=None)
+args = parser.parse_args()
+if args.game:
+    for alias in gameAliases:
+        if args.game == alias:
+            gSearch = gameAliases[alias]
+            break
+    else:
+        gSearch = args.game
+else:
+    gSearch = None
 
 def getApi():
     try:
@@ -18,9 +48,9 @@ def getApi():
     except:
         return None
 
-def populateList(apiResponse):
+def populateList(apiResponse, search):
+    printList = []
     if apiResponse != None:
-        printList = []
         for channel in apiResponse['_source']['channels']:
             game = h.unescape(channel['meta_game'])
             title = h.unescape(channel['title'])
@@ -30,32 +60,44 @@ def populateList(apiResponse):
                 url = 'http://hitbox.tv/' + channel['name']
             name = channel['display_name']
             viewers = channel['current_viewers']
-            printList.append({'game': game, 'title': title, 'url': url, 'name': name, 'viewers': viewers})
+            if search == None or search.lower() in game.lower():
+                printList.append({'game': game, 'title': title, 'url': url, 'name': name, 'viewers': viewers})
+            else:
+                pass
             finalList = sorted(printList, key=lambda k: k['viewers'], reverse=True)
         return finalList
+
     else:
         return None
 
 
 def termDisplay(pList, tHeight):
-    for channel in range(0,tHeight):
+    if len(pList) < tHeight:
+        listLength = len(pList)
+    else:
+        listLength = tHeight
+    for channel in range(0,listLength):
         print(str(int(channel) + 1) + ': \033[037m' + pList[channel]['name'] +
               '\033[0m -- '+ pList[channel]['game'] + '\n\t\033[036m' +
               pList[channel]['title'] + '\033[0m -- Viewers: ' + str(pList[channel]['viewers']))
+    return listLength
     
-pList = populateList(getApi())
-if pList != None:
-    termDisplay(pList, tHeight)
-    streamSelect = input('Select stream to watch [1-' + str(tHeight) + ']: ')
+pList = populateList(getApi(), gSearch)
+if len(pList) == 0:
+    print('No streams to display')
+elif pList != None:
+    listLength = termDisplay(pList, tHeight)
+    streamSelect = input('Select stream to watch [1-' + str(listLength) + ']: ')
     try:
         streamInt = int(streamSelect)
-        if streamInt > tHeight:
+        if streamInt > listLength:
             print('Selected stream out of range')
         elif streamInt == 0:
             print('Selected stream out of range')
         else:
             selectedStream = pList[streamInt - 1]
             print(selectedStream['url'])
+            # really not a nice way to open livestreamer but who cares
             subprocess.Popen(['livestreamer', selectedStream['url'], livestreamerQuality])
     except ValueError:
         print('Input is not a number')
